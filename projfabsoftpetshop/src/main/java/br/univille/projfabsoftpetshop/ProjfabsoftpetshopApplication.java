1a1138b9180b608ee4d12b1cc40980ca1db7e8ab
SAMUEL LEAL DOS SANTOS <samuel.santos23@univille.br>
2025-04-01 22:00:40 -0300

    main

---
+++ projfabsoftpetshop/src/main/java/br/univille/projfabsoftpetshop/ProjfabsoftpetshopApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftpetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjfabsoftpetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjfabsoftpetshopApplication.class, args);
+	}
+
+}
---
+++ projfabsoftpetshop/src/test/java/br/univille/projfabsoftpetshop/ProjfabsoftpetshopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoftpetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjfabsoftpetshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
