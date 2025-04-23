d699b63505cc0f1897b88ef63ea46c6bd9ff7938
SAMUEL LEAL DOS SANTOS <samuel.santos23@univille.br>
2025-04-22 22:04:38 -0300

    SalvandoCliente

--- projfabsoftpetshop/src/main/java/br/univille/projfabsoftpetshop/controller/ClienteController.java
+++ projfabsoftpetshop/src/main/java/br/univille/projfabsoftpetshop/controller/ClienteController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.univille.projfabsoftpetshop.entity.Cliente;
 import br.univille.projfabsoftpetshop.service.ClienteService;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/api/v1/clientes")
@@ -21,5 +26,39 @@ public class ClienteController {
         var listaClientes = service.getAll();
 
         return new ResponseEntity<List<Cliente>>(listaClientes, HttpStatus.OK);      
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> postCliente(@RequestBody Cliente cliente){
+        if(cliente == null){
+            return ResponseEntity.badRequest().build();
+        }
+        if(cliente.getId() == 0){
+            service.save(cliente);
+            return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
+        }
+        return ResponseEntity.badRequest().build();
+
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente>
+        putCliente(@PathVariable long id,
+                    @RequestBody Cliente cliente){
+        if(id <= 0 || cliente == null){
+            return ResponseEntity.badRequest().build();
+        }
+        var clienteAntigo = service.getById(id);
+        if(clienteAntigo == null){
+            return ResponseEntity.notFound().build();
+        }             
+
+        clienteAntigo.setNome(cliente.getNome());
+        clienteAntigo.setEndereco(clienteAntigo.getEndereco());
+        clienteAntigo.setPets(clienteAntigo.getPets());
+
+        service.save(clienteAntigo);
+        return new ResponseEntity<Cliente>(clienteAntigo, HttpStatus.OK);
     }
 }
