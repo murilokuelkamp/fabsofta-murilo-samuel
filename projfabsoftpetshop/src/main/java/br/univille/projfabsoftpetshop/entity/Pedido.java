731e34b15ab8f9ea47fd4e140b418da2d12d7bcd
MURILO HENRIQUE DE OLIVEIRA KUELKAMP <murilo.kuelkamp@univille.br>
2025-05-14 19:18:01 -0300

    finalizado

--- projfabsoftpetshop/src/main/java/br/univille/projfabsoftpetshop/entity/Pedido.java
+++ projfabsoftpetshop/src/main/java/br/univille/projfabsoftpetshop/entity/Pedido.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,7 +25,7 @@ public class Pedido {
     @ManyToOne
     private Cliente cliente;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     private List<Produto> produtos = new ArrayList<>();
 
     @OneToOne(cascade = CascadeType.ALL)
