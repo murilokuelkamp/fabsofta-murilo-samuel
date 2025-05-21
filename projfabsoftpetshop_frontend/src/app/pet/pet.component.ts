03597da97d0c29c679ae7030062f10a04d1026c9
Murilo Kuelkamp <murilo.henrique2410@gmail.com>
2025-05-21 23:34:46 +0000

    tela

---
+++ projfabsoftpetshop_frontend/src/app/pet/pet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet',
+  imports: [],
+  templateUrl: './pet.component.html',
+  styleUrl: './pet.component.css'
+})
+export class PetComponent {
+
+}
